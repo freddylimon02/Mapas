2a55b187ef9beb78e9d9de590121c94157ad0f3a
Freddy Limon <freddylimon02@gmail.com>
2026-05-21 18:24:23 -0400

    first commit

---
+++ init/01-postgis.sql
@@ -0,0 +1,11 @@
+-- habilita soporte para :
+-- - Tipos de geometria
+-- - Funciones espaciales
+-- - Indices espaciales
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+-- - Extencion de topologia espacial
+-- - Validacion de geometria avanzada
+-- - redes espaciales
+
+CREATE EXTENSION IF NOT EXISTS postgis_topology;
